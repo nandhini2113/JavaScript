3e7a7088681b13a120b89fabdab86f718fb6621b
nandhini2113 <126754870+nandhini2113@users.noreply.github.com>
2024-10-27 20:34:59 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,22 @@
+let person = {
+    name : "miller",
+    age : 25,
+    city : "New York"
+}
+
+keys = Object.keys(person);
+values = Object.values(person);
+entries = Object.entries(person);
+
+console.log(keys);
+console.log(values);
+console.log(entries);
+
+console.log(person['name']);
+console.log(person.age);
+
+
+
+
+
+
